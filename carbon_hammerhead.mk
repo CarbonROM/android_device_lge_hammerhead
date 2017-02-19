c8aef516eff9da686072ce6c1df04b792aaeee77
vikrant699 <vikrantarya1@gmail.com>
2017-02-19 01:22:31 +0100

    hammerhead: add device maintainer
    Change-Id: Ibb2f4465c2067485ad7e862653230e6871d3ffd7

--- carbon_hammerhead.mk
+++ carbon_hammerhead.mk
@@ -11,13 +11,17 @@ $(call inherit-product, vendor/carbon/config/gsm.mk)
 # Inherit device configuration
 $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
 
-## Device identifier. This must come after all inclusions
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hammerhead
 PRODUCT_NAME := carbon_hammerhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Device Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.carbon.maintainer="Vikrant Arya"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
